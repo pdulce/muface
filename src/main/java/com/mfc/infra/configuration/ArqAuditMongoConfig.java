08f718455e26850778e7a0aa420bf44d7982b36c
pedro.dulce <pedro.dulce@gmail.com>
2024-06-22 17:55:36 +0200

    New division entre mongos ddbb

--- src/main/java/com/mfc/infra/configuration/ArqAuditMongoConfig.java
+++ src/main/java/com/mfc/infra/configuration/ArqAuditMongoConfig.java
@@ -5,8 +5,12 @@ import org.springframework.boot.autoconfigure.mongo.MongoProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
@@ -15,15 +19,16 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
         mongoTemplateRef = "arqAuditMongoTemplate"
 )
 public class ArqAuditMongoConfig {
+
     @Bean(name = "arqAuditMongoTemplate")
-    public MongoTemplate arqAuditMongoTemplate() {
+    public MongoTemplate arqAuditMongoTemplate(MongoCustomConversions customConversions) {
         MongoProperties mongoProperties = arqAuditMongoProperties();
-        return new MongoTemplate(
-                new SimpleMongoClientDatabaseFactory(
-                        MongoClients.create(mongoProperties.getUri()),
-                        mongoProperties.getDatabase()
-                )
-        );
+        MongoDatabaseFactory myMongoDbFactory = new SimpleMongoClientDatabaseFactory(
+                MongoClients.create(mongoProperties.getUri()), mongoProperties.getDatabase());
+        MappingMongoConverter converter = new MappingMongoConverter(myMongoDbFactory, new MongoMappingContext());
+        converter.setCustomConversions(customConversions);
+        converter.afterPropertiesSet();
+        return new MongoTemplate(myMongoDbFactory, converter);
     }
 
     @Bean(name = "arqAuditMongoProperties")
--- src/main/java/com/mfc/infra/configuration/ArqMongoDBConfig.java
+++ src/main/java/com/mfc/infra/configuration/ArqMongoDBConfig.java
@@ -4,11 +4,14 @@ import com.mongodb.client.MongoClients;
 import org.springframework.boot.autoconfigure.mongo.MongoProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
+
 
 public class ArqMongoDBConfig {
 
@@ -21,14 +24,17 @@ public class ArqMongoDBConfig {
 
     @Primary
     @Bean(name = "bdMongoTemplate")
-    public MongoTemplate bdMongoTemplate() {
+    public MongoTemplate bdMongoTemplate(MongoCustomConversions custombdMongoConversions) {
         MongoProperties mongoProperties = bdMongoProperties();
-        return new MongoTemplate(
-                new SimpleMongoClientDatabaseFactory(
-                        MongoClients.create(mongoProperties.getUri()),
-                        mongoProperties.getDatabase()
-                )
-        );
+        MongoDatabaseFactory myMongoDbFactory = new SimpleMongoClientDatabaseFactory(
+                MongoClients.create(mongoProperties.getUri()), mongoProperties.getDatabase());
+        MappingMongoConverter converter = new MappingMongoConverter(myMongoDbFactory, new MongoMappingContext());
+        converter.setCustomConversions(custombdMongoConversions);
+        converter.afterPropertiesSet();
+
+        return new MongoTemplate(myMongoDbFactory, converter);
     }
+
+
 
 }
