02119f8b5a834d04fd466b0545e5f9c5e7987bab
pedro.dulce <pedro.dulce@gmail.com>
2024-06-22 16:27:27 +0200

    New division entre mongos ddbb

---
+++ src/main/java/com/mfc/infra/controller/ArqAuditoriasControllerAPI.java
@@ -0,0 +1,92 @@
+package com.mfc.infra.controller;
+
+import com.mfc.infra.event.futuro.output.ArqEventStoreInputPort;
+import com.mfc.infra.event.futuro.saga.ArqSagaOrchestratorPort;
+import jakarta.validation.constraints.NotEmpty;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Locale;
+
+@RestController
+@RequestMapping(value = "auditorias")
+public class ArqAuditoriasControllerAPI {
+    Logger logger = LoggerFactory.getLogger(ArqAuditoriasControllerAPI.class);
+    @Autowired
+    protected MessageSource messageSource;
+
+    @Autowired(required=false)
+    protected ArqSagaOrchestratorPort orchestratorManager;
+    @Autowired(required=false)
+    protected ArqEventStoreInputPort auditoriasAdapter;
+
+    @Autowired(required=false)
+    protected ArqEventStoreInputPort sagaAdapter;
+
+    /** ENDPOINTS QUE DAN ACCESO A LA INFORMACIÓN DE CUALQUIER AUDITORIA DE CUALQUIER APLICACION **/
+
+    @GetMapping(value = "{applicationId}", produces= MediaType.APPLICATION_JSON_VALUE)
+    public List<Object> getAllFromEventStoreCustomers(@PathVariable @NotEmpty String applicationId) {
+        return this.auditoriasAdapter.findAllByApp(applicationId);
+    }
+
+    @GetMapping(value = "{applicationId}/{almacen}", produces= MediaType.APPLICATION_JSON_VALUE)
+    public List<Object> getAllFromEventStoreCustomers(@PathVariable @NotEmpty String applicationId,
+                                                                   @PathVariable @NotEmpty String almacen) {
+        return this.auditoriasAdapter.findAllByAppAndStore(applicationId, almacen);
+    }
+
+    @GetMapping(value = "{applicationId}/{almacen}/{idAgregado}",
+            produces=MediaType.APPLICATION_JSON_VALUE)
+    public Object getAllEventsFromIdFromRedis(@PathVariable @NotEmpty String applicationId,
+                                                    @PathVariable @NotEmpty String almacen,
+                                                    @PathVariable @NotEmpty String idAgregado) {
+        return this.auditoriasAdapter.findAggregateByAppAndStoreAndAggregateId(applicationId, almacen, idAgregado);
+    }
+
+    /** ENDPOINTS QUE DAN ACCESO A LA INFORMACIÓN DE CUALQUIER TRANSACCION EN CUALQUIER APLICACION **/
+
+    @GetMapping(value = "transacciones-distribuidas/{applicationId}", produces=MediaType.APPLICATION_JSON_VALUE)
+    public List<Object> getAllTransactionsOfApplication(@PathVariable @NotEmpty String applicationId,
+                                                                     @PathVariable @NotEmpty String saga) {
+        return this.sagaAdapter.findAllByApp(applicationId);
+    }
+
+    @GetMapping(value = "transacciones-distribuidas/{applicationId}/{saga}", produces=MediaType.APPLICATION_JSON_VALUE)
+    public List<Object> getAllTransactionsOfSagaInApp(@PathVariable @NotEmpty String applicationId,
+                                                                   @PathVariable @NotEmpty String saga) {
+        return this.sagaAdapter.findAllByAppAndStore(applicationId, saga);
+    }
+
+    @GetMapping(value = "transacciones-distribuidas/{applicationId}/{saga}/{transactionId}",
+            produces=MediaType.APPLICATION_JSON_VALUE)
+    public Object getAllStepsInSagaTransactionId(@PathVariable @NotEmpty String applicationId,
+                                                       @PathVariable @NotEmpty String saga,
+                                                       @PathVariable @NotEmpty String transactionId) {
+        return this.sagaAdapter.findAggregateByAppAndStoreAndAggregateId(applicationId, saga, transactionId);
+    }
+
+
+    @GetMapping(value = "transacciones-distribuidas/{applicationId}/{saga}/{transactionId}/status")
+    private String getSagaEstadoFinalizacion(@PathVariable @NotEmpty String applicationId,
+                                             @PathVariable @NotEmpty String saga,
+                                             @PathVariable @NotEmpty String transactionId) {
+        logger.info("...Comprobamos si finalizó la transacción number: " + transactionId + " de la saga " + saga);
+        String[] messageKeyAndArgs = this.orchestratorManager.
+                getLastStateOfTansactionInSaga(applicationId, saga, transactionId);
+        Object[] args = new Object[messageKeyAndArgs.length - 1];
+        for (int i = 1; i < messageKeyAndArgs.length; i++) {
+            args[i-1] = messageKeyAndArgs[i];
+        }
+        return messageSource.getMessage(messageKeyAndArgs[0], args, new Locale("es"));
+    }
+
+}
