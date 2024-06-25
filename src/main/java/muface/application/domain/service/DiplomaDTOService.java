9b686174adeac3588856057f6001e27f21ab1c44
pedro.dulce <pedro.dulce@gmail.com>
2024-06-25 21:34:49 +0200

    mejoras

--- src/main/java/muface/application/domain/service/DiplomaDTOService.java
+++ src/main/java/muface/application/domain/service/DiplomaDTOService.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 @Service
 public class DiplomaDTOService extends ArqGenericService<DiplomaDTO, Long> {
